56d763f9d7670452d706dc65947453a9fb1ad56d
Philipp280 <62957215+Philipp280@users.noreply.github.com>
2025-10-27 12:26:41 +0100

    Update config.js

--- .tina/config.js
+++ .tina/config.js
@@ -4,13 +4,28 @@ export default defineConfig({
   branch: "main",
   clientId: "ce719b37-c77c-4c2d-8125-510392897570",
   token: "",
-  build: { outputFolder: "admin", publicFolder: "static" },
-  media: { tina: { mediaRoot: "images", publicFolder: "static" } },
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "images",
+      publicFolder: "static",
+    },
+  },
   schema: {
     collections: [
-      { name: "home", label: "Startseite", path: "content", match: { include: ["_index.md"] }, format: "md", fields: [{ name: "title", label: "Titel", type: "string" }, { name: "body", label: "Inhalt", type: "rich-text" }] },
-      { name: "mixes", label: "Mixes", path: "content/mixes", format: "md", fields: [{ name: "title", label: "Titel", type: "string" }, { name: "body", label: "Inhalt", type: "rich-text" }] },
-      { name: "book", label: "Buchen", path: "content/book", format: "md", fields: [{ name: "title", label: "Titel", type: "string" }, { name: "body", label: "Inhalt", type: "rich-text" }] }
+      {
+        name: "pages",
+        label: "Seiten",
+        path: "content",
+        format: "md",
+        fields: [
+          { name: "title", label: "Titel", type: "string" },
+          { name: "body", label: "Inhalt", type: "rich-text" },
+        ],
+      },
     ],
   },
 });
